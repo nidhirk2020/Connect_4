bc757e531abab10ca9239a6a7b017287c6959afe
Nidhi <nidhirk2020@gmail.com>
2022-09-13 23:45:02 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.connect_4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.connect_4 to javafx.fxml;
+    exports com.example.connect_4;
+}
