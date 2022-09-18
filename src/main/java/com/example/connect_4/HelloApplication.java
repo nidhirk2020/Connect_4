f33ebaae64a6ecdede4f429e377694ff6869d70d
Nidhi <nidhirk2020@gmail.com>
2022-09-18 13:45:25 +0530

    Connect_4 Game

--- src/main/java/com/example/connect_4/HelloApplication.java
+++ src/main/java/com/example/connect_4/HelloApplication.java
@@ -1,21 +1,109 @@
 package com.example.connect_4;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private HelloController controller ;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        GridPane rootGridPane = fxmlLoader.load();
+
+        MenuBar menuBar = createMenu();
+        Pane menuPane= (Pane) rootGridPane.getChildren().get(0);
+        menuPane.getChildren().add(menuBar);
+        menuBar.prefWidthProperty().bind(stage.widthProperty());
+         controller=fxmlLoader.getController();
+         controller.createPlayGround();
+        Scene scene = new Scene(rootGridPane);
+        stage.setTitle("Connect Four");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
     }
+
+    private MenuBar createMenu(){
+        //file Menu
+        Menu file = new Menu("File");
+        MenuItem newGame = new MenuItem("newGame");
+
+        newGame.setOnAction(actionEvent -> controller.resetGme());
+
+        MenuItem resetGame = new MenuItem("resetGame");
+
+        resetGame.setOnAction(actionEvent -> controller.resetGme());
+
+        SeparatorMenuItem  SeparatorMenuItem =new  SeparatorMenuItem();
+        MenuItem exitGame = new MenuItem("exitGame");
+
+        exitGame.setOnAction(actionEvent -> exitGame());
+
+        file.getItems().addAll(newGame,resetGame,SeparatorMenuItem,exitGame);
+       //Help menu
+        Menu help = new Menu("Help");
+        MenuItem aboutGame = new MenuItem("aboutGame");
+
+        aboutGame.setOnAction(actionEvent -> aboutGame());
+
+
+        SeparatorMenuItem  SeparatorMenuItem1 =new  SeparatorMenuItem();
+        MenuItem aboutMe = new MenuItem("aboutMe");
+        aboutMe.setOnAction(actionEvent -> aboutMe());
+
+
+        help.getItems().addAll(aboutGame,SeparatorMenuItem1,aboutMe);
+
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(file,help);
+
+       return menuBar;
+
+    }
+
+    private void aboutMe() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About the Developer");
+        alert.setHeaderText("Nidhi Verma");
+        alert.setContentText("I am an engineering student "+
+                " In my free time I love to code and create Games "+
+                " and Connect 4 is one such game. "+
+                " Hope you enjoy playing it ");
+        alert.show();
+    }
+
+    private void aboutGame() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About connect 4");
+        alert.setHeaderText("How to Play?");
+        alert.setContentText("Connect Four is a two-player connection game in which the players first choose a color and then take turns dropping"+
+                "colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying"+
+                "the next available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or"+
+                "diagonal line of four of one's own discs. Connect Four is a solved game. The first player can always win by playing the right moves.");
+        alert.show();
+
+    }
+
+    private void exitGame() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/connect_4/HelloController.java
+++ src/main/java/com/example/connect_4/HelloController.java
@@ -1,14 +1,238 @@
 package com.example.connect_4;
 
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.util.Duration;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class HelloController implements Initializable {
+    private static final int COLUMNS =7;
+    private static final int ROWS =6;
+    private static final int CIRCLE_DIAMETER =80;
+    private static final String DISC_COLOR1="#00008B";
+    private static final String DISC_COLOR2="#FF69B4";
+
+    private static String Player_One="Player1";
+    private  static String Player_two="Player2";
+
+    private boolean isPlayerOneTurn = true;
+
+    private boolean isAllowedToInsert =true;
+
+    public void createPlayGround(){
+        Shape RectangleWithHoles = new Rectangle((COLUMNS+1) * CIRCLE_DIAMETER ,(ROWS+1)*CIRCLE_DIAMETER);
+        for(int row=0;row<ROWS;row++){
+            for(int col=0;col<COLUMNS;col++){
+                Circle circle =new Circle();
+                circle.setRadius(CIRCLE_DIAMETER/2);
+                circle.setCenterX(CIRCLE_DIAMETER/2);
+                circle.setCenterY(CIRCLE_DIAMETER/2);
+                circle.setSmooth(true);
+                circle.setTranslateX(col*(CIRCLE_DIAMETER+5) + CIRCLE_DIAMETER/4);
+                circle.setTranslateY(row*(CIRCLE_DIAMETER+5)+ CIRCLE_DIAMETER/4);
+                RectangleWithHoles = Shape.subtract(RectangleWithHoles,circle);
+
+
+
+            }
+
+        }
+        RectangleWithHoles.setFill(Color.WHITE);
+        rootGridPane.add(RectangleWithHoles,0,1);
+        List<Rectangle> rectangleList = CreateClickableColumns();
+        for (Rectangle rectangle: rectangleList) {
+            rootGridPane.add(rectangle,0,1);
+        }
+
+    }
+
+
+    private Disc [][] insertedDiscsArray = new Disc[ROWS][COLUMNS];
+
+
+
+
+    private List<Rectangle> CreateClickableColumns(){
+
+        List <Rectangle> rectangleList = new ArrayList<>();
+        for(int col=0;col<COLUMNS;col++) {
+            Rectangle rectangle = new Rectangle(CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
+            rectangle.setFill(Color.TRANSPARENT);
+            rectangle.setTranslateX(col*(CIRCLE_DIAMETER+5)+CIRCLE_DIAMETER / 4);
+            rectangle.setOnMouseEntered(event -> rectangle.setFill(Color.valueOf("#eeeeee26")));
+            rectangle.setOnMouseExited(event -> rectangle.setFill(Color.TRANSPARENT));
+            final int column =col;
+
+            rectangle.setOnMouseClicked(event -> {
+                if(isAllowedToInsert) {
+                    isAllowedToInsert=false;
+                    insertDiscs(new Disc(isPlayerOneTurn), column);
+                }
+            });
+
+            rectangleList.add(rectangle);
+        }
+        return rectangleList;
+    }
+
+    private void insertDiscs(Disc disc, int column) {
+        int row =ROWS-1;
+        while(row>=0){
+            if(discIfPresent(row,column) == null){
+                break;
+            }
+           row--;
+            if(row<0){
+                return ;
+            }
+        }
+        insertedDiscsArray [row][column] = disc;
+        insertedDiscsPane.getChildren().add(disc);
+        int currentRow =row;
+        disc.setTranslateX(column*(CIRCLE_DIAMETER+5)+CIRCLE_DIAMETER / 4);
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5),disc);
+        translateTransition.setToY(row*(CIRCLE_DIAMETER+5)+CIRCLE_DIAMETER / 4);
+        translateTransition.setOnFinished(event -> {
+            isAllowedToInsert=true;
+            if(gameEnded (currentRow,column)){
+
+                gameOver();
+
+            }
+            isPlayerOneTurn = !isPlayerOneTurn;
+            playerNameLabel.setText(isPlayerOneTurn ? Player_One : Player_two);
+        });
+         translateTransition.play();
+    }
+
+    private void gameOver() {
+        String winner = isPlayerOneTurn ? Player_One : Player_two;
+        System.out.println("Winner is: "+ winner);
+
+        Alert alert =new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Connect Four");
+        alert.setHeaderText("The winner is "+winner);
+        alert.setContentText("Want to play again ?");
+
+        ButtonType yesBtn = new ButtonType("Yes");
+
+        ButtonType noBtn = new ButtonType("No, Exit");
+        alert.getButtonTypes().setAll(yesBtn , noBtn);
+
+        Platform.runLater(()->{
+            Optional<ButtonType> btnClicked = alert.showAndWait();
+            if(btnClicked.isPresent() && btnClicked.get()== yesBtn) {
+                resetGme();
+            }
+            else{
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+    }
+
+    public void resetGme() {
+      insertedDiscsPane.getChildren().clear();
+        for (int row = 0; row < insertedDiscsArray.length; row++) {
+            for (int col = 0; col < insertedDiscsArray[row].length; col++){
+                insertedDiscsArray[row][col]=null;
+            }
+
+        }
+        isPlayerOneTurn =true;
+        playerNameLabel.setText(Player_One);
+        createPlayGround();
+    }
+
+    private boolean gameEnded(int row, int column) {
+     List<Point2D> verticalPoints  = IntStream.rangeClosed(row-3,row+3).mapToObj(r->new Point2D(r,column)).collect(Collectors.toList());
+     List<Point2D> horizontalPoints  = IntStream.rangeClosed(column-3,column+3).mapToObj(col->new Point2D(row,col)).collect(Collectors.toList());
+     Point2D startPoint1 = new Point2D(row-3 , column+3);
+     List<Point2D> diagonalPoints   = IntStream.rangeClosed(0,6).mapToObj(i->startPoint1.add(i,-i)).collect(Collectors.toList());
+        Point2D startPoint2 = new Point2D(row-3 , column+3);
+     List<Point2D> diagonal2Points   = IntStream.rangeClosed(0,6).mapToObj(i->startPoint2.add(i,i)).collect(Collectors.toList());
+
+     boolean isEnded = checkCombination (verticalPoints) || checkCombination(horizontalPoints) || checkCombination(diagonalPoints) || checkCombination(diagonal2Points);
+        return isEnded;
+    }
+
+    private boolean checkCombination(List<Point2D> points) {
+        int chain=0;
+        for (Point2D point : points) {
+
+            int rowIndexForArray = (int) point.getX();
+            int columnIndexForArray =(int)  point.getY();
+            Disc disc = discIfPresent(rowIndexForArray,columnIndexForArray);
+            if(disc !=null && disc.isPlayerOneMove == isPlayerOneTurn){
+                chain++;
+                if(chain==4){
+                    return true;
+                }
+            }
+            else{
+                chain=0;
+            }
+        }
+        return false;
+    }
+
+    private Disc discIfPresent (int row , int column){
+        if(row>=ROWS || row<0 || column>=COLUMNS || column<0)
+          return null;
+
+         return insertedDiscsArray[row][column];
+    }
+
+    private static class Disc extends Circle {
+        private final boolean isPlayerOneMove;
+        public Disc(boolean isPlayerOneMove){
+          this.isPlayerOneMove =isPlayerOneMove;
+          setRadius(CIRCLE_DIAMETER/2);
+          setFill(isPlayerOneMove ? Color.valueOf(DISC_COLOR1) : Color.valueOf(DISC_COLOR2));
+          setCenterX(CIRCLE_DIAMETER/2);
+          setCenterY(CIRCLE_DIAMETER/2);
+
+        }
+    }
+
+    @FXML
+    public GridPane rootGridPane;
+    @FXML
+    public Pane insertedDiscsPane;
+    @FXML
+    public Pane menuPane;
+    @FXML
+    public Label playerNameLabel;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
